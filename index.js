da766ed8085e2f6f8b0d72ca0633cb8c720e26bc
Emma Perlman <emmajperlman@gmail.com>
2022-03-15 13:35:24 -0700

    commit tree 3bdda3659f42f8e46f1a44c12ff547a508dd6122

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+function createEmployeeRecord(array){
+    return {
+            firstName:array[0],
+            familyName:array[1],
+            title: array[2],
+            payPerHour: array[3],
+            timeInEvents:[],
+            timeOutEvents:[]
+    }
+}
+
+function createEmployeeRecords(array){
+    return array.map(e=>createEmployeeRecord(e))
+}
+
+function createTimeInEvent(record,stamp){
+    record.timeInEvent.push({
+                type:'TimeIn',
+                hour: parseInt(stamp.substring(11,15)),
+                date: stamp.substring(0,9)
+            })
+    return record
+}
+// createTimeInEvent({name:'Emma'},'2022-03-14 1200')
+
